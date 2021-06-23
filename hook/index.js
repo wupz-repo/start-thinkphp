7e9d93ab4ef03b5b0d7a3c9c79e9882b1920d4bf
wss-git <wssgryx@163.com>
2021-06-23 10:30:02 +0800

    init repo

---
+++ hook/index.js
@@ -0,0 +1,27 @@
+async function preInit(inputObj) {
+
+}
+
+async function postInit(inputObj) {
+    console.log(`
+    __  .__    .__        __           .__            
+    _/  |_|  |__ |__| ____ |  | ________ |  |__ ______  
+    \   __\  |  \|  |/    \|  |/ /\____ \|  |  \\____ \ 
+     |  | |   Y  \  |   |  \    < |  |_> >   Y  \  |_> >
+     |__| |___|  /__|___|  /__|_ \|   __/|___|  /   __/ 
+               \/        \/     \/|__|        \/|__|         
+                                        `)
+    console.log(`\n    Welcome to the start-thinkphp application
+     This application requires to open these services: 
+         FC : https://fc.console.aliyun.com/
+         ACR: https://cr.console.aliyun.com/
+     This application can help you quickly deploy the Laravel project:
+         Full yaml configuration: https://github.com/devsapp/thinkphp
+         thinkphp development docs : https://docs.topthink.com/think-api/1835085
+     This application homepage: https://github.com/devsapp/start-thinkphp\n`)
+}
+
+module.exports = {
+    postInit,
+    preInit
+}
